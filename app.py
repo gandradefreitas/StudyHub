45e2481e538b9918712962da9ffe18c911a528c4
Guilherme De Andrade <franciscoguilhermedeandradedea@gmail.com>
2026-07-28 10:31:22 -0300

    feat: primeira versão funcional do StudyHub

--- app.py
+++ app.py
@@ -1,20 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def pagina_inicial():
     return render_template("pagina_inicial.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def pagina_login():
+
+    if request.method == "POST":
+
+        email = request.form.get("email")
+        senha = request.form.get("senha")
+
+        print(email)
+        print(senha)
+
     return render_template("pagina_login.html")
 
 
-@app.route("/cadastro")
+@app.route("/cadastro", methods=["GET", "POST"])
 def pagina_cadastro():
+
+    if request.method == "POST":
+
+        nome = request.form.get("nome")
+        email = request.form.get("email")
+        senha = request.form.get("senha")
+        confirmar_senha = request.form.get("confirmar_senha")
+
+        print(nome)
+        print(email)
+        print(senha)
+        print(confirmar_senha)
+
     return render_template("pagina_cadastro.html")
 
 
